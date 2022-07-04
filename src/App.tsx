8d904a435333b54a6c4782c9286247fb1351b957
Kiana <kianashui@gmail.com>
2022-07-03 18:38:19 -0700

    added navbar, some pages, and routing to pages from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Trips from "./pages/Trips";
+import AccountSettings from "./pages/AccountSettings";
+import More from "./pages/More";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <main>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/trips" element={<Trips />} />
+            <Route path="/settings" element={<AccountSettings />} />
+            <Route path="/more" element={<More />} />
+          </Routes>
+        </Router>
+      </main>
+      <footer>
+        <p> &copy;2022, Kiana Shui</p>
+      </footer>
     </div>
   );
 }
---
+++ src/pages/AccountSettings.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AccountSettings() {
+    return (
+    <div className="account-settings">
+        <h1>Account Settings</h1>
+    </div>
+    );
+}
+
+export default AccountSettings;
