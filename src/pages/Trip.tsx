999abd2a64eac1a2121b52cafd001f956d448a18
Kiana <kianashui@gmail.com>
2022-07-25 14:35:30 -0700

    show some trip data on each individual trip's page

--- src/pages/Trip.tsx
+++ src/pages/Trip.tsx
@@ -2,6 +2,7 @@ import {useParams} from "react-router-dom";
 import {useState, useEffect} from 'react';
 import axios from "axios";
 
+
 function Trip() {
     const defaultTrip = {
         _id: "",
@@ -13,13 +14,23 @@ function Trip() {
     const [trip, setTrip] = useState(defaultTrip);
     const tripId = useParams().tripId;
     const URL = "https://capstone-trip-planner.herokuapp.com/trips";
+    
+    const getOneTrip = () => {
+		axios.get(`${URL}/${tripId}`).then((response) => {
+			const tripData = response.data[0];
+			setTrip(tripData);
+		}).catch((err) => {console.log(err);});
+	}
+
+    useEffect(getOneTrip, [tripId]);
 
     return (
         <div className="trip">
-            <h2>Trip ID: {tripId}</h2>
-            
-            {/* <h2>Trip Name: {trip.name} </h2> */}
-            {/* <h2>Trip Data: {trip} </h2> */}
+            <h2>Trip ID: {trip._id}</h2>
+            <h2>Trip Name: {trip.name} </h2>
+            <h2>Start Date: {trip.start_date} </h2>
+            <h2>End Date: {trip.end_date} </h2>
+            <h2>Itinerary Entries: {trip.itinerary_entries}</h2>
         </div>
     );
 }
