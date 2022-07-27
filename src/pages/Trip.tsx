0959cd3858ace5b5c236f07c8aba8941d38c181d
Kiana <kianashui@gmail.com>
2022-07-27 13:05:07 -0700

    changed spacing to 2 spaces per tab

--- src/pages/Trip.tsx
+++ src/pages/Trip.tsx
@@ -4,35 +4,35 @@ import axios from "axios";
 
 
 function Trip() {
-    const defaultTrip = {
-        _id: "",
-        name: "",
-        start_date: "",
-        end_date: "",
-        itinerary_entries: []
-    }
-    const [trip, setTrip] = useState(defaultTrip);
-    const tripId = useParams().tripId;
-    const URL = "https://capstone-trip-planner.herokuapp.com/trips";
-    
-    const getOneTrip = () => {
-		axios.get(`${URL}/${tripId}`).then((response) => {
-			const tripData = response.data[0];
-			setTrip(tripData);
-		}).catch((err) => {console.log(err);});
-	}
+  const defaultTrip = {
+    _id: "",
+    name: "",
+    start_date: "",
+    end_date: "",
+    itinerary_entries: []
+  }
+  const [trip, setTrip] = useState(defaultTrip);
+  const tripId = useParams().tripId;
+  const URL = "https://capstone-trip-planner.herokuapp.com/trips";
+  
+  const getOneTrip = () => {
+    axios.get(`${URL}/${tripId}`).then((response) => {
+      const tripData = response.data[0];
+      setTrip(tripData);
+    }).catch((err) => {console.log(err);});
+}
 
-    useEffect(getOneTrip, [tripId]);
+  useEffect(getOneTrip, [tripId]);
 
-    return (
-        <div className="trip">
-            <h2>Trip ID: {trip._id}</h2>
-            <h2>Trip Name: {trip.name} </h2>
-            <h2>Start Date: {trip.start_date} </h2>
-            <h2>End Date: {trip.end_date} </h2>
-            <h2>Itinerary Entries: {trip.itinerary_entries}</h2>
-        </div>
-    );
+  return (
+    <div className="trip">
+      <h2>Trip ID: {trip._id}</h2>
+      <h2>Trip Name: {trip.name} </h2>
+      <h2>Start Date: {trip.start_date} </h2>
+      <h2>End Date: {trip.end_date} </h2>
+      <h2>Itinerary Entries: {trip.itinerary_entries}</h2>
+    </div>
+  );
 }
 
 export default Trip;
